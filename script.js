e5c63c3e98fd20536fdb0138dfc039e6dcbe41b9
MadMax934 <nithin.rapolu77@gmail.com>
2025-04-30 23:12:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let currentPage = 1;
+function nextPage(n) {
+  document.getElementById(`page${currentPage}`).classList.add('hidden');
+  document.getElementById(`page${n}`).classList.remove('hidden');
+  currentPage = n;
+}
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById('page1').classList.remove('hidden');
+});

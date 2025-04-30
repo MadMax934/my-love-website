8ac6f70502febba1db040a93fa2f6e9798c5b3a7
Nithin <nithin.rapolu77@gmail.com>
2025-05-01 01:43:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 let currentPage = 1;
+
 function nextPage(n) {
   document.getElementById(`page${currentPage}`).classList.add('hidden');
   document.getElementById(`page${n}`).classList.remove('hidden');
   currentPage = n;
 }
+
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById('page1').classList.remove('hidden');
+  document.getElementById("page1").classList.remove("hidden");
 });
